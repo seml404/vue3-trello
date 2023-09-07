bed439f56ff1708a2057bf210340e6f7caa2c4ec
seml <4040591@gmail.com>
2023-09-07 17:13:52 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <p>trello</p>
+  </header>
+</template>
+
+<style scoped></style>
