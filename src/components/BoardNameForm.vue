64cf87a68a366dd66f9e1a57ff6f19ae2e2128d4
seml <4040591@gmail.com>
2023-09-28 16:43:12 +0300

    worked on board

--- src/components/BoardNameForm.vue
+++ src/components/BoardNameForm.vue
@@ -1,8 +1,11 @@
 <template>
-  <div>
-    <h1 class="px-3 py-1.5 text-2xl text-white font-bold">{{ newBoard.name }}</h1>
-    <form action="">
+  <div class="flex flex-col items-start">
+    <h1 class="px-3 py-1.5 text-2xl text-white font-bold" ref="boardHeading">
+      {{ newBoard.name }}
+    </h1>
+    <form action="" class="">
       <input
+        ref="boardInput"
         v-model="newBoard.name"
         type="text"
         placeholder="boardname"
@@ -13,10 +16,16 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 const props = defineProps({
   board: { type: Object, required: true }
 })
-
 const newBoard = ref({ ...props.board })
+const boardHeading = ref()
+const boardInput = ref()
+
+watch(newBoard.value, () => {
+  console.log(boardInput.value.style.width)
+  boardInput.value.style.width = `${boardHeading.value.offsetWidth}px`
+})
 </script>
